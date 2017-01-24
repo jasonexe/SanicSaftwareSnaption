9ed64d52ec82627bdd7d8b96823b7ed5c73d7362
jasonexe <jkrein@calpoly.edu>
2017-01-24 10:38:58 -0800

    Changing the upload thing to use callbacks instead of creating the dialog inside the uploader class

--- SnaptionApp/app/src/main/java/com/snaptiongame/snaptionapp/servercalls/FirebaseUploader.java
+++ SnaptionApp/app/src/main/java/com/snaptiongame/snaptionapp/servercalls/FirebaseUploader.java
@@ -27,22 +27,15 @@ import com.snaptiongame.snaptionapp.models.User;
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
-
 /**
  * Created by jason_000 on 1/21/2017.
  */
 
 public class FirebaseUploader implements Uploader{
-    //TODO pull this from a file instead
-    public static final String storageBucket = "gs://vertical-prototype-81b3e.appspot.com";
     private static final String usersPath = "usersSnaption";
     private static final String captionPath = "captions";
     private static final String gamesPath = "games";
-    private static final int progressDivisor = 1000;
     public static final String imagePath = "images";
-    private Context context;
-    private View view;
 
     private static FirebaseDatabase database = FirebaseDatabase.getInstance();
 
@@ -56,15 +49,23 @@ public class FirebaseUploader implements Uploader{
         myRef.removeValue();
     }
 
-    public FirebaseUploader(Context context, View view) {
-        this.context = context;
-        this.view = view;
+    public FirebaseUploader() {
+
+    }
+
+    public interface UploadDialogInterface {
+
+        public void onStartUpload(long maxBytes);
+
+        public void onUploadProgress(long bytes);
+
+        public void onUploadDone();
     }
 
     @Override
-    public void addGame(Game game, byte[] photo) {
+    public void addGame(Game game, byte[] photo, UploadDialogInterface uploadCallback) {
         //TODO notify invited players
-        uploadPhoto(game, photo);
+        uploadPhoto(game, photo, uploadCallback);
 
         // Add gameId to user's gamesList
         addGameToUserTable(game);
@@ -125,42 +126,36 @@ public class FirebaseUploader implements Uploader{
         });
     }
 
-    private void uploadPhoto(Game game, byte[] photo) {
+
+
+    private void uploadPhoto(Game game, byte[] photo, final UploadDialogInterface uploadCallback) {
         // Upload photo to storage
         FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference imageLoc = storage.getReferenceFromUrl(storageBucket)
+        StorageReference imageLoc = storage.getReference()
                 .child(imagePath + "/" + game.getId() + ".jpg");
         UploadTask uploadTask = imageLoc.putBytes(photo);
         //Creating the progress dialog
-        final ProgressDialog loadingDialog = new ProgressDialog(context);
-        loadingDialog.setIndeterminate(false);
-        loadingDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        loadingDialog.setProgress(0);
-        loadingDialog.setProgressNumberFormat("%1dKB/%2dKB");
-        loadingDialog.setMessage("Uploading photo");
-        loadingDialog.setMax(photo.length/progressDivisor);
-        //Display progress dialog
-        loadingDialog.show();
+        uploadCallback.onStartUpload(photo.length);
+
 
         uploadTask.addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                loadingDialog.hide();
+                uploadCallback.onUploadDone();
                 e.printStackTrace();
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                 // When done uploading, hide the progress bar
-                loadingDialog.hide();
+                uploadCallback.onUploadDone();
             }
         }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                uploadCallback.onUploadProgress(taskSnapshot.getBytesTransferred());
                 //Display progress as it updates
-                loadingDialog.setProgress((int) taskSnapshot.getBytesTransferred()/progressDivisor);
+
             }
         });
     }
