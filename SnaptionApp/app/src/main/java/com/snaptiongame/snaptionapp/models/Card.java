3dd1eeba103381b7c07e9beb7e888d597feb7930
Cameron Geehr <camgeehr@gmail.com>
2017-02-06 14:55:46 -0800

    Made Card and Caption serializable

--- SnaptionApp/app/src/main/java/com/snaptiongame/snaptionapp/models/Card.java
+++ SnaptionApp/app/src/main/java/com/snaptiongame/snaptionapp/models/Card.java
@@ -1,12 +1,14 @@
 package com.snaptiongame.snaptionapp.models;
 
+import java.io.Serializable;
+
 /**
  * Class to keep track of card info. When populating cardText, any blanks should be reperesented
  * by a %s modifier (I think modifier is the right word)
  * @author Jason Krein
  */
 
-public class Card {
+public class Card implements Serializable {
     public String id;
     public String cardText;
 
