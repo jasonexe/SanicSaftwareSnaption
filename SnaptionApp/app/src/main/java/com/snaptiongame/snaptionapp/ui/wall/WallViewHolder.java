d8d931140a64b0bba74ece701093d3ef6e5352ef
brittanyberlanga <brittany.berlanga@gmail.com>
2017-03-13 11:53:25 -0700

    Wall item redesign (#95)
    * Updated wall item
    
    * Fixed xml to allow for build
    
    * Changed upvote icon
    
    * Added long press to create a game from an existing game

--- SnaptionApp/app/src/main/java/com/snaptiongame/snaptionapp/ui/wall/WallViewHolder.java
+++ SnaptionApp/app/src/main/java/com/snaptiongame/snaptionapp/ui/wall/WallViewHolder.java
@@ -1,9 +1,9 @@
 package com.snaptiongame.snaptionapp.ui.wall;
 
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.snaptiongame.snaptionapp.R;
@@ -16,22 +16,22 @@ import butterknife.ButterKnife;
  */
 
 public class WallViewHolder extends RecyclerView.ViewHolder {
-    @BindView(R.id.captioner_text)
-    public TextView captionerText;
+    @BindView(R.id.picker_name)
+    public TextView pickerName;
     @BindView(R.id.photo)
     public ImageView photo;
     @BindView(R.id.caption_text)
     public TextView captionText;
-    @BindView(R.id.picker_name)
-    public TextView pickerName;
-    @BindView(R.id.picker_photo)
-    public ImageView pickerPhoto;
-    @BindView(R.id.create_from_existing_button)
-    public FloatingActionButton createFromExisting;
+    @BindView(R.id.captioner_container)
+    public LinearLayout captionerLayout;
+    @BindView(R.id.captioner_name)
+    public TextView captionerName;
+    @BindView(R.id.captioner_photo)
+    public ImageView captionerPhoto;
     @BindView(R.id.upvote_icon)
     public ImageView upvoteIcon;
-    @BindView(R.id.number_upvotes_text)
-    public TextView numberUpvotesText;
+    @BindView(R.id.upvote_count)
+    public TextView upvoteCountText;
 
 
     public WallViewHolder(View itemView) {
