720d208aaf024b311fe166c1f486cca107420aa5
jasonexe <jasonexe@users.noreply.github.com>
2017-04-17 19:36:10 -0700

    Game page optimization (#121)
    * Changing how captions are listened to to use a child event listener
    instead. It's still slow, I believe because of the way user images are
    loaded (if those are removed, it's pretty quick). Will need further
    investigation
    
    * Adding user loading optimization
    
    * Removed stress testing code
    
    * Fixing broken tests
    
    * FIxing broken test again
    
    * Getting rid of debug text, using notifyItemChanged instead of
    setCaptionerView

--- SnaptionApp/app/src/main/java/com/snaptiongame/snaption/servercalls/EventListenCreator.java
+++ SnaptionApp/app/src/main/java/com/snaptiongame/snaption/servercalls/EventListenCreator.java
@@ -58,37 +58,42 @@ public class EventListenCreator {
         };
     }
 
-    public static ChildEventListener getChildEventListener(final Class type, final ResourceListener listener) {
+    public static ChildEventListener getChildEventListener(final Class type, final ChildResourceListener listener) {
         //create ChildEventListener that wont throw exception if database has corrupted data
         return new ChildEventListener() {
             @Override
-            public void onChildAdded (DataSnapshot dataSnapshot, String s){
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 try {
-                    listener.onData(dataSnapshot.getValue(type));
+                    listener.onNewData(dataSnapshot.getValue(type));
                 } catch (Exception err) {
                     FirebaseReporter.reportException(err, "Crash trying to get data from Firebase. Type: " + type.toString());
-                    listener.onData(null);
+                    listener.onNewData(null);
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                try {
+                    listener.onDataChanged(dataSnapshot.getValue(type));
+                } catch (Exception err) {
+                    FirebaseReporter.reportException(err, "Crash trying to get data from Firebase. Type: " + type.toString());
+                    listener.onNewData(null);
                 }
             }
 
             @Override
-            public void onChildChanged (DataSnapshot dataSnapshot, String s){
-                // No op, not updating yet.
-                // TODO Will put stuff in here once upvotes happen, probably
-            }
-
-            @Override
-            public void onChildRemoved (DataSnapshot dataSnapshot){
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
                 // No op, don't need to do anything if a comment is removed
             }
 
             @Override
-            public void onChildMoved (DataSnapshot dataSnapshot, String s){
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
                 // No op, comments won't be moved.
             }
 
             @Override
-            public void onCancelled (DatabaseError databaseError){
+            public void onCancelled(DatabaseError databaseError) {
                 // No op for now.
             }
         };
