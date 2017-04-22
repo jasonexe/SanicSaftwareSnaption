ce3d161ad67b7cc1bc270b2b12767b8c3e52eee6
Austin Robarts <austinrobarts1@gmail.com>
2017-04-22 16:51:57 -0700

    Creating new classes needed for new structure

---
+++ SnaptionApp/app/src/main/java/com/snaptiongame/snaption/models/UserPublicData.java
@@ -0,0 +1,48 @@
+package com.snaptiongame.snaption.models;
+
+import java.util.Map;
+
+/**
+ * UserPublicData holds all data that can be publicly shared from a user to the community.
+ * This includes friends, captions on public games, and public games the user created
+ *
+ * Created by austinrobarts on 4/22/17.
+ */
+
+public class UserPublicData {
+    private Map<String, Caption> captions; //captions user has added to public games
+    private Map<String, Integer> createdGames; //public games user has created
+    private Map<String, Integer> friends; //friends the user has added
+
+    public UserPublicData() {}
+
+    public UserPublicData(Map<String, Caption> captions, Map<String, Integer> createdGames, Map<String, Integer> friends) {
+        this.captions = captions;
+        this.createdGames = createdGames;
+        this.friends = friends;
+    }
+
+    public Map<String, Caption> getCaptions() {
+        return captions;
+    }
+
+    public Map<String, Integer> getCreatedGames() {
+        return createdGames;
+    }
+
+    public Map<String, Integer> getFriends() {
+        return friends;
+    }
+
+    public void setCaptions(Map<String, Caption> captions) {
+        this.captions = captions;
+    }
+
+    public void setCreatedGames(Map<String, Integer> createdGames) {
+        this.createdGames = createdGames;
+    }
+
+    public void setFriends(Map<String, Integer> friends) {
+        this.friends = friends;
+    }
+}
