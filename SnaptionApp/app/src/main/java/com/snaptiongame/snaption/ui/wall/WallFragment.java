996b2d1b7ab15e0121a866ea09e3caf3cf5a7618
jasonexe <jkrein@calpoly.edu>
2017-05-11 13:33:47 -0700

    Fixing games loading too early because of wrong calculations

--- SnaptionApp/app/src/main/java/com/snaptiongame/snaption/ui/wall/WallFragment.java
+++ SnaptionApp/app/src/main/java/com/snaptiongame/snaption/ui/wall/WallFragment.java
@@ -45,7 +45,7 @@ public class WallFragment extends Fragment {
     private static final String GAME_TYPE = "game_type";
     private static final int NUM_COLUMNS = 2;
     private static final int SCROLL_DOWN_CONST = 1;
-    private static final double PERCENT_BEFORE_LOAD = .1; // Load games if 10 % is left to scroll
+    private static final double PERCENT_BEFORE_LOAD = .9; // Load games if 10 % is left to scroll
     private Unbinder unbinder;
     private WallViewAdapter wallAdapter;
     private boolean isLoading = false;
@@ -163,10 +163,10 @@ public class WallFragment extends Fragment {
                 if(!isLoading && wallListView != null) {
                     // offset + extent = range
                     int offset = wallListView.computeVerticalScrollOffset();
-                    int range = wallListView.computeVerticalScrollRange();
-                    double extentBeforeLoad = wallListView.computeVerticalScrollExtent() * PERCENT_BEFORE_LOAD;
-                    int scrolledSoFar = offset + range;
-                    if(scrolledSoFar > extentBeforeLoad) {
+                    int extent = wallListView.computeVerticalScrollExtent();
+                    double rangeBeforeLoad = wallListView.computeVerticalScrollRange()*PERCENT_BEFORE_LOAD;
+                    int scrolledSoFar = offset + extent;
+                    if(scrolledSoFar > rangeBeforeLoad) {
                         loadMoreGames();
                     }
                 }
