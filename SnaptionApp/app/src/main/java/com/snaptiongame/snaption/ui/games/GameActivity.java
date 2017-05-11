353ea713cc95b6ca53d4cf91ed22e2b1bf055a22
brittanyberlanga <brittany.berlanga@gmail.com>
2017-05-11 10:45:42 -0700

    SANIC-214 bug fix (#162)
    * Added scroll listener to minimize view behavior to hide fab on scroll
    
    * Moved onscrolllistener to a separate file
    
    * Small unimportant change
    
    * Added check to remove scroll listener when the game is closed

--- SnaptionApp/app/src/main/java/com/snaptiongame/snaption/ui/games/GameActivity.java
+++ SnaptionApp/app/src/main/java/com/snaptiongame/snaption/ui/games/GameActivity.java
@@ -298,7 +298,7 @@ public class GameActivity extends HomeAppCompatActivity {
         startCommentManager(game.getMetaData());
     }
 
-    private void loadPhoto(GameMetadata metadata) {
+    private void loadPhoto(final GameMetadata metadata) {
         // set the progress bar and image view height using the image aspect ratio
         Resources res = getResources();
         final int imageHeight = ViewUtilities.calculateViewHeight(metadata.getImageAspectRatio(),
@@ -318,7 +318,7 @@ public class GameActivity extends HomeAppCompatActivity {
                             progressBar.setVisibility(View.GONE);
                             // add a new behavior to the image view
                             minimizeImageBehavior = new MinimizeViewBehavior(gameContentLayout,
-                                    imageHeight);
+                                    imageHeight, metadata.isOpen() ? new HideFabOnScrollListener(fab) : null);
                             ((CoordinatorLayout.LayoutParams) imageView.getLayoutParams())
                                     .setBehavior(minimizeImageBehavior);
                             // start transition now that image is loaded
